d84f04f1ebf73e6af886e270759b34c0c8cda970
Jain4556 <sunil925business@gmail.com>
2026-02-21 18:17:04 +0530

    middleware_added

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,51 @@
-import { clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server'
+import { auth, clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { url } from 'inspector';
+import { URL } from 'next/dist/compiled/@edge-runtime/primitives/url';
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware()
+
+const isPublicRoute = createRouteMatcher([
+    "/signin",
+    "/signup",
+    "/",
+    "/home"
+])
+
+const isPublicApiRoute = createRouteMatcher([
+    "/api/vidoes"
+])
+
+
+
+
+export default clerkMiddleware((auth, req) => {
+    const {userId} = auth();
+    const currentUrl = new URL(req.url)
+
+    const isAccessingDashboard = currentUrl.pathname === "/home"
+    const isApiRequest = currentUrl.pathname.startsWith("/api")
+
+
+    if (userId && isPublicRoute(req) && !isAccessingDashboard) {
+        return NextResponse.redirect(new URL("/home", req.url))
+    }
+
+    // not loggeddIn
+    if (!userId) {
+        if (!isPublicRoute(req) && !isPublicApiRoute(req) ) {
+                return NextResponse.redirect(new URL("/signin", req.url))
+        }
+        if (isApiRequest && !isPublicApiRoute(req)) {
+            return NextResponse.redirect(new URL("/signin", req.url))
+        }
+        
+    }
+
+    return NextResponse.next()
+
+    
+})
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+    matcher: 
 }
