9def827957964ad10445751d1750f5c4660e33b0
Jain4556 <sunil925business@gmail.com>
2026-02-22 16:48:08 +0530

    prisma_updated

--- app/api/videos/route.ts
+++ app/api/videos/route.ts
@@ -1,22 +1,23 @@
 import { NextRequest, NextResponse } from "next/server"
+
+
 import { PrismaClient } from "@prisma/client"
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-const prisma = globalForPrisma.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+const prisma = new PrismaClient()
 
 export async function GET(request: NextRequest) {
     try {
         const videos = await prisma.video.findMany({
             orderBy: { createdAt: "desc" }
         })
+
         return NextResponse.json(videos)
     } catch (error) {
-        return NextResponse.json(
-            { error: "Error fetching videos" },
-            { status: 500 }
-        )
+        return NextResponse.json({ error: "Error fetching    " },
+            { status: 500 })
     }
+    // finally{
+    //     await prisma.$disconnect()
+    // }
+
 }
